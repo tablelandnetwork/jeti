b3c8ff289e25d22c5a8ac914b038cd113f77d043
Allen Muncy <allen@textile.io>
2023-04-21 14:32:54 -0400

    Update commands

--- src/resolve.ts
+++ src/resolve.ts
@@ -1,37 +1,47 @@
 import { globalIpfsClient } from "./ipfs-http-client-setup.js";
 
-interface ResultSet {
-  columns: Array<any>;
-  rows: Array<any>;
+// New type definitions
+interface ResultRow {
+  [key: string]: string | number;
 }
 
+interface ResultSet {
+  [index: number]: ResultRow;
+}
+
+interface IPFSResolveResultRow {
+  [key: string]: string | number | AsyncIterable<Uint8Array>;
+}
+
+interface IPFSResolveResultSet {
+  [index: number]: IPFSResolveResultRow;
+}
+
+// Updated function
 async function resolve(
   resultSet: ResultSet,
-  columnsToResolve: [string]
-): Promise<ResultSet> {
+  columnsToResolve: string[]
+): Promise<IPFSResolveResultSet> {
   const ipfs = await globalIpfsClient;
-  const resolveColumnIndex: Array<number> = [];
-  resultSet.columns.forEach((column, key) => {
-    if (columnsToResolve.includes(column.name)) {
-      resolveColumnIndex.push(key);
-    }
-  });
+  const resolveColumnNames: Set<string> = new Set(columnsToResolve);
 
-  const resolveSet = resultSet;
+  const resolveSet: IPFSResolveResultSet = {};
 
-  resolveSet.rows = resultSet.rows.map(async (row) => {
-    const resolvingRow = row.map((cell: any, key: number) => {
-      if (resolveColumnIndex.includes(key)) {
-        return ipfs.cat(cell);
+  for (const index in resultSet) {
+    const row = resultSet[index];
+    const resolvingRow: IPFSResolveResultRow = {};
+
+    for (const key in row) {
+      if (resolveColumnNames.has(key)) {
+        resolvingRow[key] = await ipfs.cat(row[key] as any);
+      } else {
+        resolvingRow[key] = row[key];
       }
-      return cell;
-    });
-    return resolvingRow;
-  });
+    }
 
-  resolveSet.rows = await Promise.all(resolveSet.rows);
+    resolveSet[index] = resolvingRow;
+  }
 
   return resolveSet;
 }
-
 export { resolve };
